e54b23e72866194a921daf4a23475d14426ab801
samshigvan <samshigvan182@gmail.com>
2024-08-09 16:05:03 +0530

    max sub array

---
+++ src/com/company/DSA/Array/MaxSubArray.java
@@ -0,0 +1,24 @@
+package com.company.DSA.Array;
+
+//https://leetcode.com/problems/maximum-subarray/
+public class MaxSubArray {
+
+    public static int maxSubArray(int[] nums) {
+
+        int len = nums.length;
+        int sum = nums[0];
+        int maxSum = nums[0];
+
+        for(int end =1; end<len; end++){
+            sum = Math.max(nums[end], sum + nums[end]);
+            maxSum = Math.max(sum, maxSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int []arr= {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(arr));
+    }
+
+}
