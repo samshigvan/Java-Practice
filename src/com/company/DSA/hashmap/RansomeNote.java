9fef4f1bedae2b6ac384b07717e11a05a3c06215
samshigvan <samshigvan182@gmail.com>
2024-08-20 20:04:58 +0530

    solved- solved ransome note using char array

--- src/com/company/DSA/hashmap/RansomeNote.java
+++ src/com/company/DSA/hashmap/RansomeNote.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class RansomeNote {
 
-    public static boolean canConstruct(String ransomNote, String magazine) {
+    public static boolean canConstruct2(String ransomNote, String magazine) {
 
         HashMap<Character, Integer> map = new HashMap<>();
 
@@ -23,6 +23,23 @@ public class RansomeNote {
                 return false;
             }
         }
+        return true;
+    }
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] count = new int[26];
+
+        for (char ch : magazine.toCharArray()){
+            count[ch-'a'] = count[ch-'a'] +1; // count[ch-'a']++;
+        }
+
+        for (char ch : ransomNote.toCharArray()){
+
+            if(count[ch-'a']==0) return false;
+            count[ch-'a']--;
+        }
+
         return true;
     }
 
