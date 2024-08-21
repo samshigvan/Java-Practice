414a3db6cce3275f5c65f7d257a76140491bb990
samshigvan <samshigvan182@gmail.com>
2024-08-21 12:15:04 +0530

    solved- solved is palindrome campared using ASCII value

--- src/com/company/DSA/Two_pointer/ValidPalindrome.java
+++ src/com/company/DSA/Two_pointer/ValidPalindrome.java
@@ -3,7 +3,41 @@ package com.company.DSA.Two_pointer;
 //https://leetcode.com/problems/valid-palindrome/description/
 public class ValidPalindrome {
 
-    public static boolean isPalindrome(String s) {
+    public static boolean isPalindrome(String s){
+
+        int start =0;
+        int end = s.length()-1;
+
+        while (start<end){
+            int left = isValidChar(s.charAt(start));
+            int right = isValidChar(s.charAt(end));
+
+            if (left == -1)
+                start++;
+            else if (right == -1)
+                end--;
+            else if (left != right)
+                return false;
+            else {
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+    public static int isValidChar(char c){
+
+        if( c >= '0' && c<='9')
+            return c;
+        else if (c>='a' && c<= 'z') {
+            return c;
+        } else if (c >= 'A' && c<= 'Z') {
+            return c - 'A' + 'a';
+        }
+        return -1;
+    }
+
+    public static boolean isPalindrome2(String s) {
 
         s = s.replaceAll(" ", "");
         s = s.replaceAll("[^A-Za-z0-9]", "");
@@ -22,8 +56,9 @@ public class ValidPalindrome {
     }
 
     public static void main(String[] args) {
-//        String s="A man, a plan, a canal: Panama";
-        String s = "race a car";
+        String s="A man, a plan, a canal: Panama";
+//        String s = "race a car";
+//        String s = "0a";
         System.out.println(isPalindrome(s));
     }
 }
