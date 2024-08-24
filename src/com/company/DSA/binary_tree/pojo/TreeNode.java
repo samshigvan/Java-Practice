b5e472624994042e0186bb1a5ecc9edfa27f02c1
samshigvan <samshigvan182@gmail.com>
2024-08-25 00:15:13 +0530

    created TreeNode

---
+++ src/com/company/DSA/binary_tree/pojo/TreeNode.java
@@ -0,0 +1,28 @@
+package com.company.DSA.binary_tree.pojo;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(){}
+    public TreeNode(int val){this.val=val;}
+    public TreeNode(int val, TreeNode left, TreeNode right){
+        this.val=val;
+        this.left=left;
+        this.right=right;
+    }
+
+    @Override
+    public String toString() {
+        String temp = val + " ";
+
+        if (left != null)
+            temp += left.toString();
+
+        if (right != null)
+            temp += right.toString();
+
+        return temp;
+    }
+}
