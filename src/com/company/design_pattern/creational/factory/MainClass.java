7075b9394b81d8d7d81298d7ce45022686eddc76
Sameer <samshigvan182@gmail.com>
2024-11-06 12:28:24 +0530

    Comment added for factory design pattern

--- src/com/company/design_pattern/creational/factory/MainClass.java
+++ src/com/company/design_pattern/creational/factory/MainClass.java
@@ -1,8 +1,12 @@
 package com.company.design_pattern.creational.factory;
 
 public class MainClass {
-
+    /*
+    * When we want to create objects based on certain conditions
+    * then we can use Factory design patter
+    * */
     public static void main(String[] args) {
+
 
         VehicleFactory factory = new VehicleFactory();
         Vehicle vehicle = factory.getVehicle("Luxury");
